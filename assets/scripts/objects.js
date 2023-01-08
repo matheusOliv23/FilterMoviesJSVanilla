cb886128d3000a44be82c0597c3f8402a0418820
matheusOliv23 <matheushenri1996@gmail.com>
2023-01-08 18:01:41 -0300

    feat: add filter function

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -3,7 +3,7 @@ const searchBtn = document.getElementById('search-btn')
 
 const movies = [];
 
-const renderMovies = () => {
+const renderMovies = (filter = '') => {
     const movieList = document.getElementById('movie-list')
 
     if (movies.length === 0) {
@@ -14,9 +14,19 @@ const renderMovies = () => {
 
     movieList.innerHTML = ''
 
-    movies.forEach((movie) => {
+    const filteredMovies = !filter ?
+        movies :
+        movies.filter(movie => movie.info.title.includes(filter))
+
+    filteredMovies.forEach((movie) => {
         const movieElement = document.createElement('li')
-        movieElement.textContent = movie.info.title;
+        let text = movie.info.title + ' - '
+        for (const key in movie.info) {
+            if (key !== 'title') {
+                text = text + `${key}: ${movie.info[key]}`
+            }
+        }
+        movieElement.textContent = text
         movieList.append(movieElement)
     })
 }
@@ -45,4 +55,10 @@ const addMovieHandler = () => {
     renderMovies()
 }
 
+const searchMovieHandler = () => {
+    const filterTerm = document.getElementById('filter-title').value
+    renderMovies(filterTerm)
+}
+
 addMovieBtn.addEventListener('click', addMovieHandler)
+searchBtn.addEventListener('click', searchMovieHandler)
